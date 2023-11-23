7b76ac19e6470d15c959eb870816f10e90898b56
mohan <msy061618@gmail.com>
2023-11-23 20:37:37 +0530

    php file added

---
+++ php_crud/php_crud/db.php
@@ -0,0 +1,17 @@
+<?php
+//server with default setting (user 'root' with no password)
+$host = '192.168.153.57';  // server 
+$user = 'root';   
+$pass = "12345678";   
+$database = 'demodb';   //Database Name  
+
+// establishing connection
+  $conn = mysqli_connect($host,$user,$pass,$database);   
+
+ // for displaying an error msg in case the connection is not established
+  if (!$conn) {                                             
+    die("Connection failed: " . mysqli_connect_error());     
+  }
+?>
+
+
